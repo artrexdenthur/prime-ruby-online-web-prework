76bae02c8f1d2140c94b2c1ffc78664aecfe288b
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 15:25:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(integer)
   # returns true if integer is prime, else false
-  # 
+  # 1, -1 and 0 are not prime
+  if integer
   for factor in (2..integer - 1)
     if integer % factor == 0
       return false
