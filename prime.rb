d33b2a6555dafd3fb25755a476a7a695c490a0b4
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 15:26:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 def prime?(integer)
   # returns true if integer is prime, else false
+  
   # 1, -1 and 0 are not prime
-  if integer
-  for factor in (2..integer - 1)
-    if integer % factor == 0
+  return false if integer.abs < 2 
+  
+  # 
+  for factor in (2..integer.abs - 1)
+    if integer.abs % factor == 0
       return false
     end
   end
